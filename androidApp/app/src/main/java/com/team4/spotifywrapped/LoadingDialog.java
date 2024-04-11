e7c696fbbf593ddc6d83467f97219e4fc9fea7ac
Naraja4 <ismaelzhuzhou@gmail.com>
2024-04-11 16:29:42 -0400

    Fixed minor screen mismatch

--- androidApp/app/src/main/java/com/team4/spotifywrapped/LoadingDialog.java
+++ androidApp/app/src/main/java/com/team4/spotifywrapped/LoadingDialog.java
@@ -4,31 +4,30 @@ import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.view.Window;
 import android.widget.TextView;
 
 public class LoadingDialog {
 
-    Context context;
-    Dialog dialog;
+  Context context;
+  Dialog dialog;
 
-    public LoadingDialog(Context context) {
-        this.context = context;
-    }
+  public LoadingDialog(Context context) {
+    this.context = context;
+  }
 
-    public void showDialog(String title) {
-        dialog = new Dialog(context);
-        dialog.setContentView(R.layout.loading_dialog);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+  public void showDialog(String title) {
+    dialog = new Dialog(context);
+    dialog.setContentView(R.layout.loading_dialog);
+    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-        TextView titleView = dialog.findViewById(R.id.textView);
+    TextView titleView = dialog.findViewById(R.id.textView);
 
-        titleView.setText(title);
-        dialog.create();
-        dialog.show();
-    }
+    titleView.setText(title);
+    dialog.create();
+    dialog.show();
+  }
 
-    public void hideDialog() {
-        dialog.dismiss();
-    }
+  public void hideDialog() {
+    dialog.dismiss();
+  }
 }
