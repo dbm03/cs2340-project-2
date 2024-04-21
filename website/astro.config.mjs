9f851016c416b2570e2a4c21a662e088dc1bb669
Barrarroso <correodelnino@gmail.com>
2024-04-21 14:20:28 -0400

    Added website contents

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,22 +5,14 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Spotify Wrapped Clone',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+			],
+			customCss: [
+				'./src/styles/custom.css',
 			],
 		}),
 	],
